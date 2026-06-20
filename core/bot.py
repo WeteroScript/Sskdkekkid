1b159dcdd2538580105952893e2f5afc2a0cf35d
WeteroScript <weterochina@gmail.com>
2026-06-20 19:20:03 +0300

    Delete core directory

--- core/bot.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-from config import Config
-
-bot = Bot(token=Config.BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
